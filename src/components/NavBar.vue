617f4edde70b3914bda667ff765d65ace90b72e2
Jaroslaw Romanczuk <szamotulok@gmail.com>
2025-06-08 16:04:03 +0200

    added frontend

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -58,6 +58,38 @@ export default {
     toggleSidebar() {
       this.sidebarOpen = !this.sidebarOpen;
     }
+  },
+  mounted() {
+  fetch("api/v1/")
+      .then((response) => response.text() )
+      .then((data) => {
+        this.msg = data;
+      })
+    fetch("api/v1/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          id: 2,
+          chapter_id: 3,
+          chapter_title: "tst",
+          group_id: 2
+        })
+      })
+          .then((response) => response.text())
+          .then((data) => {
+            this.msg = data;
+          })
+
+
+      fetch("api/v1/1", {
+        method: "DELETE"})
+          .then((response) => response.text())
+          .then((data) => {
+            this.msg = data;
+          })
+
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  devServer: {
+    port: 3000,
+    proxy: {
+      'api/v1/': {
+        target: 'http://localhost:8082',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
+}
